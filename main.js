02f4e9920c17ed1334592cd290dcf227787de9d3
Ivan Poch <45315570+Node4976616e@users.noreply.github.com>
2023-01-22 13:13:56 +0000

    Added new functionality

--- main.js
+++ main.js
@@ -1,46 +1,62 @@
 //Create whole random number between 2 and 11
-let firstCard = Math.floor((Math.random() * 10) + 2);
-let secondCard = Math.floor((Math.random() * 10) + 2);
+let firstCard = 0;
+let secondCard = 0;
 let newCard = 0;
+let newGame = false;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-let sum = firstCard + secondCard;
+let sum = 0;
 let showMessage = document.getElementById("show-message");
 let showCards = document.getElementById("cards");
 let showSum = document.getElementById("sum");
 
 
-if (hasBlackJack === true) {
-    alert("You have a blackjack!");
-}
 
-if (isAlive === false) {
-    alert("You are out")
-}
 
 function startGame() {
-    if (sum <= 20) {
-        message = "Do you want to draw a new card ?";
-    } else if (sum === 21) {
-        message = "You have won!";
-        hasBlackJack = true;
+    if (newGame === false) {
+        newGame = true;
+        firstCard = Math.floor((Math.random() * 10) + 2);
+        secondCard = Math.floor((Math.random() * 10) + 2);
+        sum = firstCard + secondCard;
+        showCards.textContent = `Cards: ${firstCard} ${secondCard} `;
+        showSum.textContent = `Sum: ${sum}`;
     } else {
-        message = "You've lost!";
-        isAlive = false;
+        alert("You haven't finished your game.");
     }
-    showCards.textContent = `Cards: ${firstCard} ${secondCard} `;
-    showSum.textContent = `Sum: ${sum}`;
-    showMessage.textContent = message;
 }
 
 
 
 function addNewCard() {
-    newCard = Math.floor((Math.random() * 10) + 2);
-    showCards.textContent += `${newCard}` + ` `;
-    sum += newCard;
-    showSum.textContent = `Sum: ${sum}`;
+    if (newGame === true) {
+        newCard = Math.floor((Math.random() * 10) + 2);
+        showCards.textContent += `${newCard}` + ` `;
+        sum += newCard;
+        showSum.textContent = `Sum: ${sum}`;
+        checkResults();
+    } else {
+        alert("You have lost, start new game.");
+        newGame = false;
+
+    }
 }
+
+function checkResults() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card ?";
+    } else if (sum === 21) {
+        message = "You have won!";
+        hasBlackJack = true;
+        newGame = false;
+    } else {
+        message = "You've lost!";
+        newGame = false;
+    }
+    showMessage.textContent = message;
+}
+
+
 
